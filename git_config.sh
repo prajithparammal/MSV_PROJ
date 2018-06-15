580eff3e0620aa9af84524f9c7921276cc1761b5
Prajith Parammal <prajithparammal@gmail.com>
2018-06-15 05:07:15 +0000

    Updating from azure vm putty session

---
+++ git_config.sh
@@ -0,0 +1,4 @@
+git config --global user.name 'Prajith Parammal'
+git config --global user.email prajithparammal@gmail.com
+git config --global push.default simple
+git config --global -l
---
+++ git_push.sh
@@ -0,0 +1,5 @@
+git add .
+git status
+git commit -m "Updating from azure vm putty session"
+git status
+git push
